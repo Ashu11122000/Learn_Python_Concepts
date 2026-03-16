2568f6f05ff10067fdb33fa5f385228c22fdd4c5
Ashish Sharma <ashu11vats@gmail.com>
2026-03-16 18:36:41 +0530

    Add destination.py including basic usage of comparison operator (>) and logical operator (or)

---
+++ destination.py
@@ -0,0 +1,18 @@
+# Should I walk or drive to my destination?
+# Integer variable (stores whole number)
+distance_meters = 200
+
+# Boolean variable (stores True or False)
+is_raining = False
+
+# Comparison Operator:
+# >  means "greater than"
+#
+# Logical Operator:
+# or  returns True if at least one condition is True
+#
+# Parentheses are used to group expressions
+should_drive = is_raining or (distance_meters > 1000)
+
+# print() displays output to console
+print("Should I drive?", should_drive)
