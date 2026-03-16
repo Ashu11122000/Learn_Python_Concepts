b4ad622b7d37b4d52da6547cc716441bc9ca16c8
Ashish Sharma <ashu11vats@gmail.com>
2026-03-16 19:19:41 +0530

    Add function.py file including basic function definition and calling the function

---
+++ functions.py
@@ -0,0 +1,8 @@
+# Function Definition 
+def printing(str):
+    print(str)
+    return
+
+# Now, call the function
+printing("I'm first call to user-defined function!")
+printing("Again second call to the same function!")
